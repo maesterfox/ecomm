8eae3f2534c7e32bffa8727d20b48eea13f18b86
maesterfox <talentedmrfox@hotmail.com>
2024-04-03 14:14:15 +0100

    incorporating Jest for testing
     Changes to be committed:

---
+++ test.js
@@ -0,0 +1,16 @@
+// function RemoveExclamationMarks which removes all exclamation marks from a given string
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, "");
+}
+
+// Sample Tests
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(removeExclamationMarks("Hello World!"), "Hello World");
+  });
+});
